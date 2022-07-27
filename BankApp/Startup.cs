25cb3ae2c6e2990c597db4c0e39c547bdfab4819
blitzkrieg0000 <burakhansamli0.0.0.0@gmail.com>
2022-07-27 10:28:01 +0300

    Update Startup.cs

--- BankApp/Startup.cs
+++ BankApp/Startup.cs
@@ -24,7 +24,7 @@ namespace BankApp {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddRazorPages();
+            //services.AddRazorPages();
 
             //Context in OnConfiguring kısmını dependency injection aracılığıyla yapıyoruz.
             services.AddDbContext<BankContext>(opt => {
