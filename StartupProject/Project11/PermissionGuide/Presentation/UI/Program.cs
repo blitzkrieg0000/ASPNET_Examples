cffc362735953bd6dc2835ab59877eeeb6b44289
Burakhan Şamlı <burakhansamli@proton.me>
2024-02-02 10:45:06 +0300

    .

--- StartupProject/Project11/PermissionGuide/Presentation/UI/Program.cs
+++ StartupProject/Project11/PermissionGuide/Presentation/UI/Program.cs
@@ -1,18 +1,17 @@
-namespace com.dgh.PermissionGuide;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+internal class Program {
 
 
-internal class Program
-{
-    private static void Main(string[] args){
+    private static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddRazorPages();
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment()){
+        if (!app.Environment.IsDevelopment()) {
             app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
@@ -29,4 +28,5 @@ internal class Program
 
         app.Run();
     }
+
 }
