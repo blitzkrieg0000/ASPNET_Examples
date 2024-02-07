566a369d03cd348df11ce2fac90bfedc4796b54e
Burakhan Şamlı <burakhansamli@proton.me>
2024-02-07 20:10:00 +0300

    .

--- StartupProject/Project11/PermissionGuide/Onion/Presentation/UI/Program.cs
+++ StartupProject/Project11/PermissionGuide/Onion/Presentation/UI/Program.cs
@@ -78,8 +78,9 @@ internal class Program {
 			.UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration {
 				EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
 				ValidationAlgorithm = ValidationAlgorithm.HMACSHA512
-			}).SetApplicationName("WebAppOnion01")
-		.PersistKeysToStackExchangeRedis(multiplexer, "DataProtection-Keys");
+			})
+			.SetApplicationName("WebAppOnion01")
+			.PersistKeysToStackExchangeRedis(multiplexer, "DataProtection-Keys");
 		#endregion
 
 
